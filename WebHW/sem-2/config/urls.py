1d120926163608513d0628ecd58e5775cc6da654
skyceo <aleksandrtuman@gmail.com>
2026-04-25 16:57:38 +0300

    Add template and about us (task-4)

--- WebHW/sem-2/config/urls.py
+++ WebHW/sem-2/config/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from web_hw.views import index
+from web_hw.views import about, index
 
 urlpatterns = [
     path('', index, name='home'),
+    path('about/', about, name='about'),
     path('admin/', admin.site.urls),
 ]
--- WebHW/sem-2/web_hw/views.py
+++ WebHW/sem-2/web_hw/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'web_hw/index.html')
+    context = {
+        'title': 'Главная страница',
+        'welcome_text': 'Добро пожаловать на наш лучший сайт!',
+    }
+    return render(request, 'web_hw/index.html', context)
+
+
+def about(request):
+    return render(request, 'web_hw/about.html')
